10a45ff5ed3c9dc32335d14f9ade7df0cc02999b
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2020-06-06 01:54:59 -0300

    [sorting] Feature: Insertion Sort

---
+++ go/src/sorting/insertion.go
@@ -0,0 +1,62 @@
+/*
+ * Copyright(c) 2020 Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+// Length of Array
+const arrayLength = 32
+
+// Swaps two elements in an array.
+func swap(a []int, i, j int) {
+	tmp := a[i]
+	a[i] = a[j]
+	a[j] = tmp
+}
+
+// Sorts an array.
+func insertionSort(array []int) {
+	for i := 1; i < len(array); i++ {
+		// Shift elements to rightt.
+		for j := i; j > 0; j-- {
+			if array[j-1] > array[j] {
+				swap(array, j, j-1)
+			}
+		}
+	}
+}
+
+// Tests the Insertion Sort algorithm.
+func main() {
+
+	array := make([]int, arrayLength)
+
+	// Build the input array.
+	for i := 0; i < arrayLength; i += 2 {
+		array[i] = arrayLength - i
+		array[i+1] = i + 1
+	}
+
+	fmt.Println(array)
+
+	insertionSort(array)
+
+	fmt.Println(array)
+}
