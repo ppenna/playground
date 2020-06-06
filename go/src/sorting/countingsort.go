bc4ed59e2d8fa0795a36cce38f9e326f5a0e0341
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2020-06-06 01:12:19 -0300

    [sorting] Feature: Counting Sort

---
+++ go/src/sorting/countingsort.go
@@ -0,0 +1,93 @@
+/*
+ * Copyright(c) 2020 Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package sorting
+
+import (
+	"fmt"
+)
+
+// Length of Array
+const arrayLength = 32
+
+// Maximum Number for an Integer
+const maxInt = int(^uint(0) >> 1)
+const minInt = -maxInt - 1
+
+// Finds the smallest and biggest elements of an array.
+func findMinMax(array []int) (int, int) {
+
+	min := maxInt
+	max := minInt
+
+	// Search the smallest element.
+	for i := range array {
+
+		// Smallest element found.
+		if array[i] < min {
+			min = array[i]
+		}
+
+		// Biggest element found.
+		if array[i] > max {
+			max = array[i]
+		}
+	}
+
+	return min, max
+}
+
+// Sorts an array.
+func countingSort(array []int) {
+
+	min, max := findMinMax(array)
+
+	sortedArray := make([]int, max-min+1)
+
+	// Count numbers.
+	for i := range array {
+		sortedArray[array[i]-min]++
+	}
+
+	// Rebuild array.
+	k := 0
+	for i := range sortedArray {
+		for j := 0; j < sortedArray[i]; j++ {
+			array[k] = min + i
+			k++
+		}
+	}
+}
+
+// Tests the Counting Sort algorithm.
+func Test() {
+	var array []int
+
+	array = make([]int, arrayLength)
+
+	// Build the input array.
+	for i := 0; i < arrayLength; i += 2 {
+		array[i] = arrayLength - i
+		array[i+1] = i + 1
+	}
+
+	fmt.Println(array)
+
+	countingSort(array)
+
+	fmt.Println(array)
+}
